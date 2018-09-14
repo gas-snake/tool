506fb3992320896ea60b5a534f86de112ff63a58
gas-snake <lds190@126.com>
2018-09-14 18:25:19 +0800

    33

---
+++ zhiban.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding=utf-8 
+
+users=["王源","崔伟","侯飞飞","和康宁","孔令洋","李迪生","李志勋"]
+userlist = []
+n = 5
+
+print (users[-1])
+# def movebit(n=0):
+#     for n in range(0,len(users)):
+
+
+
+# print(5%7)
+# for i in range(0,5):
+#     print ('i:',i)
+#     movenum=i%7
+
+#     print('movenum',movenum)
+
+#     userTemp=userlist[]
+#     pass 
+
